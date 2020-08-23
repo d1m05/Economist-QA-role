0b01a0b3b77d73a850e451097c47d7b741d2f847
Dimo Gkriziotis <dgkriziotis@gmail.com>
2020-08-23 23:21:14 +0100

    Implemented step definitions for home feature

--- features/step_definitions/index.js
+++ features/step_definitions/index.js
@@ -7,3 +7,25 @@ Given(/^I go to the jobs page$/, () => World.goToJobsPage());
 Then(/^I should see the navigation bar$/, async () => {
   return World.driver.findElement(By.id('primary-nav'));
 })
+
+Then('I should see the search fields', function () {
+    return World.driver.findElement(By.id('keywords'));
+})
+
+Then('I should see the sector lists', function () {
+    return World.driver.findElement(By.xpath("//span[contains(text(),'Browse jobs by Sector')]"));
+})
+
+Then('I should see the jobs blog', function () {
+    return World.driver.findElement(By.xpath("//span[contains(text(),'Jobs blog')]"));
+})
+
+Then('I should see the featured jobs', function () {
+    return World.driver.findElement(By.xpath("//span[contains(text(),'Featured jobs')]"));
+})
+
+Then('I should see the footer', function () {
+    return World.driver.findElement(By.xpath("//footer[contains(@class,'contentinfo')]"));
+})
+
+
